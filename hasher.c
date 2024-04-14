acf1148aeb87f468f8b5b8c834ef7be9d3b3f04e
Hammilk <khai1995pham@gmail.com>
2024-04-14 15:41:00 -0500

    final

--- hasher.c
+++ hasher.c
@@ -5,17 +5,19 @@
 #include<ctype.h>
 
 void print_usage(const char * app){
-  fprintf(stderr, "usage: [-h] [-s size] [-k key]\n");
+  fprintf(stderr, "usage: [-h] [-s size] [-k key] [-t type]\n");
   fprintf(stderr, "   size indicates how many characters/numbers to hash\n");
   fprintf(stderr, "   key indicates hashmap value\n");
+  fprintf(stderr, "   type indicates numeric or alphabetic (0 or 1)\n");
 }
 
 int main(int argc, char *argv[])
 {
-  const char optstr[] = "hs:k:";
+  const char optstr[] = "hs:k:t:";
   char opt;
   int hashKey = 3; //defaults
   int arrSize = 5; //defaults
+  int type = 0; //default numeric
   while((opt = getopt(argc, argv, optstr))!= -1){
     switch(opt){
       case 'h':
@@ -27,27 +29,39 @@ int main(int argc, char *argv[])
       case 'k':
         hashKey = atoi(optarg);
         break;
+      case 't':
+        type = atoi(optarg);
+        break;
       default:
         printf("Invalid options %c\n", optopt);
         print_usage(argv[0]);
         return(EXIT_FAILURE);
     } 
   }
-
+  printf("hashkey: %d\n", hashKey);
   int* arr = (int *)malloc(arrSize * sizeof(int));
 
   for(int i = 0; i < arrSize; i++){
-    char temp;
+    char temp = '1';
+    int tempInt;
+    arr[i] = 0;
     printf("Enter value[%d]: ", i);
-    scanf("%c", &temp);
-    getchar();
-    if(isalpha(temp) && temp > 96){
+    if(type == 1){
+      scanf("%c", &temp);
+      getchar();
+    }
+    else{
+      scanf("%d", &tempInt);
+    }
+    if(type == 1 && temp > 96){
       arr[i] = (temp - 96) % hashKey;
     }
-    else if(isalpha(temp)){
+    else if(type == 1){
       arr[i] = (temp - 64) % hashKey; 
     }
-    else arr[i] = (temp % hashKey);
+    else{
+      arr[i] = (tempInt % hashKey);
+    }
   }
   for(int i = 0; i < arrSize; i++){
     printf("%d\n", arr[i]);
